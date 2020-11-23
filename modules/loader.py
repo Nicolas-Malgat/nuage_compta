27ad37112009c423e566394571c011351f053f0d
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-11-23 02:10:06 +0100

    Ajout des chiffres à la reconnaissance de texte

--- modules/loader.py
+++ modules/loader.py
@@ -103,7 +103,7 @@ class ComptaDataLoader:
         print('Data extract successfully')
 
     # NOTE: PAS SUR QUE CE SOIT UTILE
-    ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     def letter_to_int(self, letter:str):
         return self.ALPHABET.index(letter)
